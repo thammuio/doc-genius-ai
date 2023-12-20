1c3aded92ed5352c3db6fd368a99f5cc980f3dff
thammuio <findsbn@gmail.com>
2023-12-20 12:18:19 -0500

    ui fixes

--- chat-ui/deploy.py
+++ chat-ui/deploy.py
@@ -1,7 +1,3 @@
 import subprocess
 
-# Define the shell script path
-deploy_script_path = 'chat-ui/deploy.sh'
-
-# Use subprocess to run the shell script
-subprocess.run(['bash', deploy_script_path], check=True)
+subprocess.run(["yarn", "start"], check=True)
--- chat-ui/setup.py
+++ chat-ui/setup.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Define the shell script path
-setup_script_path = 'chat-ui/setup.sh'
+setup_build_script_path = 'chat-ui/setup_build.sh'
 
 # Use subprocess to run the shell script
-subprocess.run(['bash', setup_script_path], check=True)
+subprocess.run(['bash', setup_build_script_path], check=True)

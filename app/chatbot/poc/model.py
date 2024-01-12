3db34dfdc5ad312b7e6e34e72b2b4185f25b5fe8
thammuio <findsbn@gmail.com>
2024-01-12 15:19:51 -0500

    fixes

--- app/chatbot/poc/model.py
+++ app/chatbot/poc/model.py
@@ -8,14 +8,21 @@ from app.embeddings.chunk_utils import *
 import warnings
 warnings.filterwarnings("ignore")
 import json
-
-
-llama2_model = load_llama_model()
+from llama_cpp import Llama
+from huggingface_hub import hf_hub_download
+from app.utils.constants import GEN_AI_MODEL_REPO, GEN_AI_MODEL_FILENAME
 
 
 # Pass through user input to LLM model with enhanced prompt and stop tokens
-
 def generate_response(json_input):
+    gen_ai_model_path = hf_hub_download(repo_id=GEN_AI_MODEL_REPO, filename=GEN_AI_MODEL_FILENAME)
+
+    llama2_model = Llama(
+        model_path=gen_ai_model_path,
+        n_gpu_layers=64,
+        n_ctx=2000
+    )
+
     try:
         data = json.loads(json_input)
         question = "Answer this question based on given context: " + data['prompt'] + " "

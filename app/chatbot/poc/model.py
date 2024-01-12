6dad45a0f9cbf3561bff402e814700183f24bb41
thammuio <findsbn@gmail.com>
2024-01-12 16:33:09 -0500

    fixes

--- app/chatbot/poc/model.py
+++ app/chatbot/poc/model.py
@@ -8,21 +8,25 @@ warnings.filterwarnings("ignore")
 import json
 from app.utils.constants import GEN_AI_MODEL_REPO, GEN_AI_MODEL_FILENAME
 
+# Download and Load model from HuggingFace Hub
+print("Downloading model from HuggingFace Hub...")
+gen_ai_model_path = hf_hub_download(repo_id=GEN_AI_MODEL_REPO, filename=GEN_AI_MODEL_FILENAME)
+
+print("Initiate Llama model...")
+llama2_model = Llama(
+    model_path=gen_ai_model_path,
+    n_gpu_layers=64,
+    n_ctx=2000
+)
+
+print("Model loaded successfully!")
 
 # Pass through user input to LLM model with enhanced prompt and stop tokens
 def generate_response(json_input):
-    print("Downloading model from HuggingFace Hub...1")
-    gen_ai_model_path = hf_hub_download(repo_id=GEN_AI_MODEL_REPO, filename=GEN_AI_MODEL_FILENAME)
-
-    print("Initiate Llama model...")
-    llama2_model = Llama(
-        model_path=gen_ai_model_path,
-        n_gpu_layers=64,
-        n_ctx=2000
-    )
 
     try:
         data = json.loads(json_input)
+        print(data)
         question = "Answer this question based on given context: " + data['prompt'] + " "
         context = " Here is the context: " + str(data['context'])
         question_and_context = question + context
@@ -33,8 +37,10 @@ def generate_response(json_input):
         }
         response = llama2_model(prompt=question_and_context, **params)
 
-        answer = response['choices'][0]['text']
-        return {"answer": answer}
+        print(response)
+
+        # answer = response['choices'][0]['text']
+        return response
     
     
     except Exception as e:

a670aaab9e4bbd0e7704408492f15b08a7456278
thammuio <findsbn@gmail.com>
2024-04-25 08:53:39 -0400

    new models

--- app/api/get_configs.py
+++ app/api/get_configs.py
@@ -8,12 +8,12 @@ def get_settings_data():
                 "link": "https://huggingface.co/meta-llama/Llama-2-13b-chat-hf"
             },
             {
-                "name": "llama-2-70b-chat",
-                "link": "https://huggingface.co/meta-llama/Llama-2-13b-chat-hf"
+                "name": "zephyr-7b-alpha",
+                "link": "https://huggingface.co/HuggingFaceH4/zephyr-7b-alpha"
             },
             {
-                "name": "Mixtral-8x7B",
-                "link": "https://huggingface.co/mistralai/Mixtral-8x7B-v0.1"
+                "name": "Meta-Llama-3-8B-Instruct",
+                "link": "https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct"
             },
             {
                 "name": "GPT-4-Turbo",

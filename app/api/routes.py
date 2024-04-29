5f86f59fef77ec060b0020471153611187b14bd3
thammuio <findsbn@gmail.com>
2024-04-29 12:27:53 -0400

    fixes

--- app/api/routes.py
+++ app/api/routes.py
@@ -23,7 +23,7 @@ def check_api_status() -> dict[str, str]:
 # Chat
 # Map the model names to the functions
 model_methods = {
-    "llama-2-7B-chat": llama_2_7b_chat
+    "llama2-7B-chat": llama_2_7b_chat
     # "llama-2-13B-chat": llama_2_13b_chat,
     # "Meta-Llama-3-8B-Instruct": llama_3_8b_instruct,
     # "zephyr-7B-alpha": zephyr_7B_alpha,
